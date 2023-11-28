af231c73b88ac6b6cbe5aa749f11123be4c6831f
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-11-28 11:15:56 +0100

    Add button to generate random quotes in person details page

--- src/app/components/persons/persons-detail/persons-detail.component.ts
+++ src/app/components/persons/persons-detail/persons-detail.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, Subject, combineLatest, map, of, startWith, switchMap } from 'rxjs';
 import { ApiService } from '../../../api.service';
 
 @Component({
@@ -8,16 +8,39 @@ import { ApiService } from '../../../api.service';
   templateUrl: './persons-detail.component.html',
   styleUrls: ['./persons-detail.component.scss']
 })
-export class PersonsDetailComponent {
+export class PersonsDetailComponent implements OnDestroy {
+  private readonly quotesSubject$ = new Subject<number | null>();
 
-  person$: Observable<any> = this.route.paramMap.pipe(
-    switchMap((params: ParamMap) =>
-      {
-        return this.apiService.getData('character/' + params.get('slug')!)}
-      )
+  personDetail$: Observable<Person[]> = this.route.paramMap.pipe(
+    switchMap((params: ParamMap) => this.apiService.getData('character/' + params.get('slug')!))
   );
+
+  quotes$: Observable<Quote[]> = this.quotesSubject$.pipe(
+    startWith(null),
+    switchMap((quoteCount) => !quoteCount ? of([]) : this.apiService.getData(`random/${quoteCount}`)
+  ));
+
+  person$: Observable<Person> = combineLatest([this.personDetail$, this.quotes$]).pipe(
+    map(([personDetail, quotes]) => {
+      const person = personDetail[0];
+
+      if(quotes.length > 0) {
+        person.quotes = quotes.map((quote: Quote) => (quote.sentence + " | " + quote.character.name))
+      }
+
+      return person
+    }));
+
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService
     ) {}
+
+  ngOnDestroy(){
+    this.quotesSubject$.complete();
+  }
+
+  generateRandomQuotes(){
+    this.quotesSubject$.next(5);
+  }
 }
